a1ef85a93a4141c768a16cc1cd457fff6d0a2ca0
Amirkaramat <amir_karamat@yahoo.com>
2023-05-17 21:55:00 +0330

    fix bug

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,5 +1,5 @@
-import React, { useContext, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import React from "react";
+import { Link} from "react-router-dom";
 import Footer from "../../Components/Footer/Footer";
 import Navbar from "../../Components/Navbar/Navbar";
 import Topbar from "../../Components/Topbar/Topbar";
