0523308b2c096a1cd80d6f5a3f7e1df4e0999c13
Amirkaramat <amir_karamat@yahoo.com>
2023-05-27 15:59:15 +0330

    update users component from admindashboard

--- client/src/Components/AdminPanel/AdminUsers/AdminUsers.js
+++ client/src/Components/AdminPanel/AdminUsers/AdminUsers.js
@@ -3,13 +3,18 @@ import DataTable from "../DataTable/DataTable";
 import Pagination from "../../Pgination/Pagination";
 import Button from "../../Form/Button/Button";
 import swal from "sweetalert";
+import { useForm } from "react-hook-form";
+import { registerSchema } from "../../../Validator/schema";
+import { yupResolver } from "@hookform/resolvers/yup";
 export default function AdminUsers() {
   const [showUser, setShowUser] = useState([]);
   const [orderedUsers, setOrderedUsers] = useState([]);
   const [usersData, serUsersData] = useState([]);
+
   useEffect(() => {
     getAllUser();
   }, []);
+
   const getAllUser = () => {
     const localStorageData = JSON.parse(localStorage.getItem("user"));
     fetch(`http://localhost:4000/v1/users`, {
@@ -36,6 +41,7 @@ export default function AdminUsers() {
         console.log(err);
       });
   };
+
   const deleteUserHandler = (userID) => {
     const localStorageData = JSON.parse(localStorage.getItem("user"));
     swal({
@@ -73,6 +79,7 @@ export default function AdminUsers() {
       }
     });
   };
+
   const banUserHandler = (userID) => {
     const localStorageData = JSON.parse(localStorage.getItem("user"));
     swal({
@@ -116,8 +123,150 @@ export default function AdminUsers() {
       }
     });
   };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(registerSchema),
+    mode: "all",
+  });
+  
+  const fromSubmitHandler = (data) => {
+    const newUser = {
+      name: data.fullName,
+      username: data.username,
+      email: data.email,
+      password: data.password,
+      confirmPassword: data.confirmPassword,
+      phone: data.phone,
+    };
+    fetch("http://localhost:4000/v1/auth/register", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newUser),
+    })
+      .then((res) => {
+        if (!res.ok) {
+          if (res.status === 409) {
+            swal({
+              title: "نام کاربری یا ایمیل تکراری است",
+              icon: "error",
+              button: "خروج",
+            });
+          } else if (res.status === 403) {
+            swal({
+              title: "این شماره تماس مسدود شده است به پشتیبانی پیام دهید",
+              icon: "error",
+              button: "خروج",
+            });
+          }
+          return res.text().then((text) => {
+            throw new Error(text);
+          });
+        } else {
+          console.log(res);
+          return res.json();
+        }
+      })
+      .then((result) => {
+        swal({
+          title: "کاربر با موفقیت ساخته شد",
+          icon: "success",
+          button: "خروج",
+        });
+      })
+      .catch((err) => {});
+  };
   return (
     <>
+      <div class='home-content-edit'>
+        <div class='back-btn'>
+          <i class='fas fa-arrow-right'></i>
+        </div>
+        <form onSubmit={handleSubmit(fromSubmitHandler)} class='form'>
+          <div class='col-6'>
+            <div class='name input'>
+              <label class='input-title'>نام و نام خانوادگی</label>
+              <input
+                type='text'
+                className=''
+                placeholder='لطفا نام و نام خانوادگی کاربر را وارد کنید...'
+                {...register("fullName")}
+              />
+              <span class='error-message text-danger'></span>
+            </div>
+          </div>
+          <div class='col-6'>
+            <div class='family input'>
+              <label class='input-title'>نام کاربری</label>
+              <input
+                type='text'
+                className=''
+                placeholder='لطفا نام کاربری را وارد کنید...'
+                {...register("username")}
+              />
+              <span class='error-message text-danger'></span>
+            </div>
+          </div>
+          <div class='col-6'>
+            <div class='email input'>
+              <label class='input-title'>ایمیل</label>
+              <input
+                type='text'
+                className=''
+                placeholder='لطفا ایمیل کاربر را وارد کنید...'
+                {...register("email")}
+              />
+              <span class='error-message text-danger'></span>
+            </div>
+          </div>
+          <div class='col-6'>
+            <div class='phone input'>
+              <label class='input-title'>شماره تلفن</label>
+              <input
+                type='text'
+                className=''
+                placeholder='لطفا شماره تلفن کاربر را وارد کنید...'
+                {...register("phone")}
+              />
+              <span class='error-message text-danger'></span>
+            </div>
+          </div>
+          <div class='col-6'>
+            <div class='password input'>
+              <label class='input-title'>رمز عبور</label>
+              <input
+                type='text'
+                className=''
+                placeholder='لطفا رمز عبور کاربر را وارد کنید...'
+                {...register("password")}
+              />
+              <span class='error-message text-danger'></span>
+            </div>
+          </div>
+          <div class='col-6'>
+            <div class='password input'>
+              <label class='input-title'>تکرار رمز</label>
+              <input
+                type='text'
+                className=''
+                placeholder='تکرار رمز عبور'
+                {...register("confirmPassword")}
+              />
+              <span class='error-message text-danger'></span>
+            </div>
+          </div>
+          <div class='col-12'>
+            <div class='bottom-form'>
+              <div class='submit-btn'>
+                <input type='submit' value='افزودن' />
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
       <DataTable title='کاربران'>
         <table className='table'>
           <thead>
